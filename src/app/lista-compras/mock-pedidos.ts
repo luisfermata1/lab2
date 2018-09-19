9643f8b370044026b73c3ac5c0574575ab5ffe28
luisfermata <luisfermata@icloud.com>
2018-09-18 23:00:57 -0600

    localStorage

--- src/app/lista-compras/mock-pedidos.ts
+++ src/app/lista-compras/mock-pedidos.ts
@@ -1,7 +1,11 @@
 import { Pedido } from './pedido';
-
-export const PEDIDOS: Pedido[]  = [
-  {name:"iPhone Xs", cant:5, color:"White Glass", capac:"256 GB", descr:"regalo1"},
-  {name:"iPhone 8", cant:3, color:"Black Glass", capac:"64 GB", descr:"regalo2"},
-  {name:"iPad", cant:8, color:"Blue Glass", capac:"256 GB", descr:"regalo3"}
+var objectArray = [
+{name:"iPhone Xs", cant:5, color:"White Glass", capac:"256 GB", descr:"regalo1"},
+{name:"iPhone 8", cant:3, color:"Black Glass", capac:"64 GB", descr:"regalo2"},
+{name:"iPad", cant:8, color:"Blue Glass", capac:"256 GB", descr:"regalo3"}
 ];
+
+localStorage.setItem('testObject', JSON.stringify(objectArray));
+var retrievedObject = localStorage.getItem('testObject');
+
+export const PEDIDOS: Pedido[]  = JSON.parse(retrievedObject);
